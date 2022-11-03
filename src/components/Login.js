19eb3183aa5d1dfcd3b19a68ddd90d06d38a3286
xstartstaticx <daniel.reiller@icloud.com>
2022-11-03 14:42:15 +0100

    added solution

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,46 @@
 import React, { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 export const Login = () => {
   const [data, setData] = useState({
     userName: "john doe",
     password: "r3@ct",
   });
-
+  const [inputData, setInputData] = useState({
+    userName: "",
+    password: "",
+    submit: false,
+  });
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (
+      inputData.userName === data.userName &&
+      inputData.password === data.password
+    ) {
+      navigate("/home");
+    } else {
+      setInputData({ ...inputData, submit: true });
+    }
+  };
+  const handleUser = (e) =>
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
   return (
     <>
       <h1>Login</h1>
-      <p>The username or password is incorrect!</p>
-      <form>
-        <input type="text" name="userName" placeholder="Username" />
-
-        <input type="password" name="password" placeholder="Password" />
-
+      {inputData.submit && <p>The user name or password is incorrect!</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleUser}
+          type="text"
+          name="userName"
+          placeholder="Username"
+        />
+        <input
+          onChange={handleUser}
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
         <input type="submit" value="Submit" />
       </form>
     </>
